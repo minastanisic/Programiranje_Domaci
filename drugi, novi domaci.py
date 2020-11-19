6d25313f86fd48d97671dea904b8287ea9ae5736
minastanisic <72164671+minastanisic@users.noreply.github.com>
2020-11-19 19:37:04 +0100

    Delete drugi, novi domaci.py

--- drugi, novi domaci.py
+++
@@ -1,8 +0,0 @@
-def spajanje():
-    str1 = input("Unesite prvi string: ")
-    str2 = input("Unesite drugi string: ")
-    str = str1 + str2
-    print(str)
-
-if __name__ == '__main__':
-    spajanje()
